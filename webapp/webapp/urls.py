85d847d13fd33d0e87be8d3e8a1e19d0ceaf204d
Ankit Kumar <nasaankit01@gmail.com>
2023-06-24 22:05:25 +0530

    redirect bug fixed

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -17,9 +17,14 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+def to_home(request):
+    return redirect("/segment")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', to_home),
     path('segment/', include('segment.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
